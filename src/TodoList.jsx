d9e47f66caf39cebceb0c579e03b840e9878852d
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-10-21 13:13:39 +0100

    Finish refactor

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,21 +1,25 @@
-export default function TodoList({ todos }) {
+import { TodoItem } from "./TodoItem"
+import PropTypes from 'prop-types';
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            completed: PropTypes.bool.isRequired,
+            title: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
+
+
+export default function TodoList({ todos, toggleTodo, deleteTodo }) {
     return (
         <ul className="list">
             {todos.length === 0 && "No Todos"}
             {todos.map(todo => {
-                return (<li key={todo.id}>
-                    <label htmlFor="">
-                        <input type="checkbox"
-                            checked={todo.completed}
-
-                        // onChange={e => toggleTodo(todo.id, e.target.checked)} 
-                        />
-                        {todo.title}
-                    </label>
-                    <button
-                        // onClick={() => deleteTodo(todo.id)}
-                        className="btn btn-danger" >Delete</button>
-                </li>)
+                return (
+                    <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+                )
             })}
         </ul>
     )
